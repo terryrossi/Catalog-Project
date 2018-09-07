847cfdea3f4e2e159c98de0c7f2e92f7fdbaf3b1
Terry-Rossi <terryrossi1@gmail.com>
2018-09-07 04:45:59 -0700

    mon first commit

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -2,14 +2,25 @@
 
 import datetime
 
-POSTS = [("This is the first post.", datetime.datetime.now())]
+import psycopg2
+
+conn = psycopg2.connect(dbname(forum)
+
+#POSTS = [("This is the first post.", datetime.datetime.now())]
 
 def get_posts():
   """Return all posts from the 'database', most recent first."""
-  return reversed(POSTS)
+  cursor = conn.cursor()
+  cursor.execute("select * from posts order by time desc")
+  POSTS = cursor.fetchall()
+  conn.close()
+  return POSTS
 
 def add_post(content):
   """Add a post to the 'database' with the current timestamp."""
-  POSTS.append((content, datetime.datetime.now()))
+  cursor = conn.cursor()
+  cursor.execute("insert into posts (content) values content")
+  conn.commit()
+  conn.close()
 
-
+#POSTS.append((content, datetime.datetime.now()))
